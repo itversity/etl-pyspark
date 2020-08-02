09ca2a98e1e3285f0b50f5e752c635a613b73b1f
itversity <dgadiraju@itversity.com>
2020-08-02 06:31:52 -0700

    Added README and refactored the query properties.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ETL Pyspark
+
+* Clone GitHub Repository
+* Create virtual environment specific to this project
+* Install dependencies
+* Activate Virtual environment
+* Launch spark-sql and create these 2 tables.
+```sql
+CREATE TABLE t (d DATE) LOCATION 'file:/Users/itversity/Projects/Internal/etl-pyspark/t';
+CREATE TABLE ts (t TIMESTAMP ) LOCATION 'file:/Users/itversity/Projects/Internal/etl-pyspark/ts';
+```
+* Make sure to create logs folder
+* Run using `spark-submit app.py REPORT_1`
--- util/sparker.py
+++ util/sparker.py
@@ -9,19 +9,21 @@ def get_spark_session():
         appName('ETL Pyspark'). \
         enableHiveSupport(). \
         getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
     return spark
 
 
 def execute_query(spark, report_name, query_name, query):
     try:
         logging.info(f'''Running query {query_name} for {report_name}''')
-        spark.sql(query['query'])
+        spark.sql(query['process.statement'])
     except Exception as e:
         logging.error(e)
         raise
 
 
 def execute_report(spark, reports, report_name):
+    logging.info(f'''Processing data for report {report_name}''')
     report_queries = reports[report_name]
     for query in report_queries:
         for key in query:
