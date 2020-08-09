eda05a57f44eba6b770aa2e85e78c9bdbed7facd
itversity <dgadiraju@itversity.com>
2020-08-09 08:16:47 -0700

    Updated and refactored

--- etl-pyspark/app.py
+++ etl-pyspark/app.py
@@ -5,6 +5,10 @@ from util.sparker import get_spark_session, execute_report
 
 
 def main():
+    if os.path.exists('jobs.zip'):
+        sys.path.insert(0, 'etl-pyspark.zip')
+    else:
+        sys.path.insert(0, './jobs')
     init_logger('.')
     report_name = sys.argv[1]
     batch_month = int(sys.argv[2])
--- etl-pyspark/create_db_objects.sql
+++ etl-pyspark/create_db_objects.sql
@@ -10,3 +10,16 @@ CREATE EXTERNAL TABLE airlines.flight_count(FlightDate DATE,
 ) PARTITIONED BY (flightmonth INT)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 LOCATION '/Users/itversity/Research/data/airlines/flight_count';
+
+CREATE EXTERNAL TABLE `airlines`.`flight_trips` (`Year` INT, `Month` INT, `DayofMonth` INT, `DayOfWeek` INT, `DepTime` STRING, `CRSDepTime` INT, `ArrTime` STRING, `CRSArrTime` INT, `UniqueCarrier` STRING, `FlightNum` INT, `TailNum` STRING, `ActualElapsedTime` STRING, `CRSElapsedTime` INT, `AirTime` STRING, `ArrDelay` STRING, `DepDelay` STRING, `Origin` STRING, `Dest` STRING, `Distance` STRING, `TaxiIn` STRING, `TaxiOut` STRING, `Cancelled` INT, `CancellationCode` STRING, `Diverted` INT, `CarrierDelay` STRING, `WeatherDelay` STRING, `NASDelay` STRING, `SecurityDelay` STRING, `LateAircraftDelay` STRING, `IsArrDelayed` STRING, `IsDepDelayed` STRING)
+PARTITIONED BY (FlightMonth INT)
+LOCATION 'dbfs:/FileStore/tables/airlines-part'
+STORED AS parquet;
+
+CREATE EXTERNAL TABLE airlines.flight_count(FlightDate DATE,
+    FlightCount INT,
+    DepDelayedCount INT,
+    ArrDelayedCount INT
+) PARTITIONED BY (flightmonth INT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+LOCATION 'dbfs:/FileStore/tables/flight_count';
